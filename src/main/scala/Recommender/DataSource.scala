fe6284b26b1902107e029886d90c3ea431a5debb
vkolmakov <vkolmakov@hotmail.com>
2016-08-31 11:09:24 -0500

    Add cosine similarity

--- src/main/scala/Recommender/DataSource.scala
+++ src/main/scala/Recommender/DataSource.scala
@@ -4,23 +4,27 @@ import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 
 object DataSource {
-    def items(from: DataSource, idPosition: Int, namePosition: Int)(implicit sc: SparkContext): RDD[Types.Item] = {
+    def items(from: DataSource, idPosition: Int, namePosition: Int)(implicit sc: SparkContext): RDD[Serialized.Item] = {
         from match {
             case File(path, delimeter) => {
                 val fields = sc.textFile(path).map(_.split(delimeter))
-                fields.map(field => (field(idPosition).toInt, field(namePosition)))
+                fields.map(field => Item(field(idPosition).toInt, field(namePosition)).serialize)
             }
         }
     }
-    def ratings(from: DataSource, userIdPosition: Int, itemIdPosition: Int, valuePosition: Int)(implicit sc: SparkContext): RDD[Types.UserRating] = {
+
+    def userRatings(from: DataSource, userIdPosition: Int, itemIdPosition: Int, valuePosition: Int)(implicit sc: SparkContext): RDD[Serialized.UserRating] = {
         from match {
             case File(path, delimeter) => {
                 val fields = sc.textFile(path).map(_.split(delimeter))
-                fields.map(field => (field(userIdPosition).toInt, (field(itemIdPosition).toInt, field(valuePosition).toDouble)))
+                fields.map(
+                    field => UserRating(User(field(userIdPosition).toInt),
+                        Rating(field(itemIdPosition).toInt, field(valuePosition).toDouble)).serialize)
             }
         }
     }
 }
 
 class DataSource
+
 case class File(path: String, delimeter: String) extends DataSource
--- src/main/scala/Recommender/Recommender.scala
+++ src/main/scala/Recommender/Recommender.scala
@@ -5,9 +5,26 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 object Recommender extends App {
-    def removeDuplicateRatings(ratingPair: (Types.User, (Types.Rating, Types.Rating))): Boolean = {
-        val (_, ((item1, _), (item2, _))) = ratingPair
+    def removeDuplicateRatings(userRatingsPair: (Serialized.User, (Serialized.Rating, Serialized.Rating))): Boolean = {
+        val (_, ((item1, _), (item2, _))) = userRatingsPair
         item1 < item2
+    }
+
+    def groupRatingPairs(userRatingsPair: (Serialized.User, (Serialized.Rating, Serialized.Rating))) = {
+        val (_, ((item1, rating1), (item2, rating2))) = userRatingsPair
+        ((item1, item2), (rating1, rating2))
+    }
+
+    def cosineSimilarity(ratingPairs: Iterable[(Double, Double)]): Serialized.Similarity = {
+        val xx = ratingPairs.map({ case (x, _) => Math.pow(x, 2) }).reduce(_ + _)
+        val yy = ratingPairs.map({ case (_, y) => Math.pow(y, 2) }).reduce(_ + _)
+        val xy = ratingPairs.map({ case (x, y) => x * y }).reduce(_ + _)
+        val count = ratingPairs.size
+
+        xy match {
+            case xy if xy != 0 => (Math.sqrt(xx) * Math.sqrt(yy) / xy, count)
+            case _ => (0, count)
+        }
     }
 
     Logger.getLogger("org").setLevel(Level.ERROR)
@@ -19,13 +36,15 @@ object Recommender extends App {
     implicit val sc = new SparkContext(sparkConfig)
 
     val itemsSource = File("./data/ml-100k/u.item", "|")
-    val items: RDD[Types.Item] = DataSource.items(itemsSource, 0, 1)
+    val items = DataSource.items(itemsSource, 0, 1)
 
-    val ratingsSource = File("./data/ml-100k/u.data", "\t")
-    val ratings: RDD[Types.UserRating] = DataSource.ratings(ratingsSource, 0, 1, 2)
-    val ratingsPairs = ratings.join(ratings).filter(removeDuplicateRatings)
+    val userRatingsSource = File("./data/ml-100k/u.data", "\t")
+    val userRatings: RDD[Serialized.UserRating] = DataSource.userRatings(userRatingsSource, 0, 1, 2)
+    val userRatingPairs = userRatings.join(userRatings).filter(removeDuplicateRatings)
+    val itemRatingPairs = userRatingPairs.map(groupRatingPairs).groupByKey()
+    val itemPairSimilarities = itemRatingPairs.mapValues(cosineSimilarity)
 
-    ratingsPairs.take(10).foreach(println)
+    itemPairSimilarities.take(10).foreach(println)
 
     sc.stop()
 }
