ac8dac43a274acc7d177b994acdb5c3fe4d845d1
vkolmakov <vkolmakov@hotmail.com>
2016-09-01 10:56:34 -0500

    Add correct dependencies + minor changes

--- build.sbt
+++ build.sbt
@@ -5,5 +5,6 @@ version := "1.0"
 scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
-    "org.apache.spark" %% "spark-core" % "2.0.0"
+    "org.apache.spark" %% "spark-core" % "2.0.0-preview",
+    "org.apache.spark" %% "spark-sql" % "2.0.0-preview"
 )
--- src/main/scala/Recommender/Recommender.scala
+++ src/main/scala/Recommender/Recommender.scala
@@ -6,9 +6,9 @@ import org.apache.spark.sql.{Dataset, SparkSession}
 
 object Recommender extends App {
     def cosineSimilarity(ratingPairs: Iterable[(Double, Double)]): Option[Similarity] = {
-        val xx = ratingPairs.map({ case (x, _) => Math.pow(x, 2) }).reduce(_ + _)
-        val yy = ratingPairs.map({ case (_, y) => Math.pow(y, 2) }).reduce(_ + _)
-        val xy = ratingPairs.map({ case (x, y) => x * y }).reduce(_ + _)
+        val xx = ratingPairs.map({ case (x, _) => Math.pow(x, 2) }).sum
+        val yy = ratingPairs.map({ case (_, y) => Math.pow(y, 2) }).sum
+        val xy = ratingPairs.map({ case (x, y) => x * y }).sum
         val count = ratingPairs.size
 
         xy match {
@@ -17,10 +17,12 @@ object Recommender extends App {
         }
     }
 
-    def getItemsRatingsPairs(userRatings: Dataset[UserRating]) = {
+    def getItemRatingPairs(userRatings: Dataset[UserRating]) = {
         import spark.implicits._
+
         val ratings = userRatings.map(ur => (ur.user.id, (ur.rating.itemId, ur.rating.value))).rdd
         val ratingPairs = ratings.join(ratings)
+
         ratingPairs.filter({ case (_, ((id1, _), (id2, _))) => id1 > id2})
             .map({ case (_, ((id1, value1), (id2, value2))) => ((id1, id2), (value1, value2))})
 
@@ -39,7 +41,7 @@ object Recommender extends App {
     val userRatingsSource = File("./data/ml-100k/u.data", "\t")
     val userRatings: Dataset[UserRating] = DataSource.userRatings(userRatingsSource, 0, 1, 2)
 
-    val itemRatingPairs = getItemsRatingsPairs(userRatings)
+    val itemRatingPairs = getItemRatingPairs(userRatings)
     val itemSimilarities = itemRatingPairs.groupByKey().flatMapValues(cosineSimilarity)
 
     spark.stop()
