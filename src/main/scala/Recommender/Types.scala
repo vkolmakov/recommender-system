ce9e83cdb5fc2ee734eeacdf47ede2a8c5d833ff
vkolmakov <vkolmakov@hotmail.com>
2016-09-02 13:17:37 -0500

    Rename file with types

--- src/main/scala/Recommender/Types.scala
+++ src/main/scala/Recommender/Types.scala
@@ -3,5 +3,4 @@ package Recommender
 final case class User(id: Int)
 final case class Item(id: Int, name: String)
 final case class Rating(itemId: Int, value: Double)
-final case class UserRating(user: User, rating: Rating)
 final case class Similarity(value: Double, strength: Int)
